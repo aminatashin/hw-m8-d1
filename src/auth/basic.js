1118be158b4c6d66305d2b10428a0cdf1034105f
Amin Atashkhayer <amin.atashin@gmail.com>
2022-05-25 13:41:51 +0200

    Update basic.js

--- src/auth/basic.js
+++ src/auth/basic.js
@@ -7,12 +7,12 @@ export const basicAuth = async (req, res, next) => {
     const cridential = req.headers.authorization.split(" ")[1];
     const [email, password] = atob(cridential).split(":");
     console.log(`email${email} password${password}`);
-  }
-  const user = await userModel.approve(email, password);
-  if (user) {
-    req.user = user;
-    next();
-  } else {
-    console.log(error);
+    const user = await userModel.approve(email, password);
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      console.log(error);
+    }
   }
 };
